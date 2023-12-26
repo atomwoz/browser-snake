b91e6e0f8872decc63b7882bbfcc5018ca9d8b49
atomwoz <atwozniak67@gmail.com>
2023-12-26 13:14:50 +0100

    Added my jq lib

---
+++ js/uposledzonejq.js
@@ -0,0 +1,24 @@
+function $random(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+function $id(id)
+{
+    return document.getElementById(id)
+}
+
+function $c(c)
+{
+    return document.getElementsByClassName(c);
+}
+
+function $mk(elem, className="")
+{
+    let x = document.createElement(elem);
+    if(className != "")
+    {
+        x.className = className
+    }
+    return x
+}
